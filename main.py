f814a6c9edc8ffbf48c8f4a638838f02e1191bcb
Seniz <so9@njit.edu>
2025-03-26 20:41:14 -0400

    Chore: added logging messages and config

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+import sys
 import logging
 import argparse
 import qrcode
 import validators
 
+def initialize_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[logging.StreamHandler(sys.stdout)]
+    )
 
 def is_valid_url(url):
     if validators.url(url):
         return True
     else:
+        logging.error(f"Invalid URL: {url}")
         return False
 
 def generate_qr_code(url, file_path):
     if not is_valid_url(url):
-        print("Invalid URL")
+        logging.warning("Invalid URL entered in command line")
         return
     
     try:
@@ -24,12 +32,15 @@ def generate_qr_code(url, file_path):
         code_img = qr.make_image(fill_color="black", back_color="white")
 
         code_img.save(file_path)
+        logging.info(f"QR code saved to {file_path}")
 
     except Exception as e:
         logging.error(f"Error while generating QR code: {e}")
 
 
 def main():
+    initialize_logging()
+    
     parser = argparse.ArgumentParser(description="Generates QR code with encoded URL")
     parser.add_argument('--url', help="The URL to encode in the QR code", default="https://github.com/atwoodmachine")
     args = parser.parse_args()
